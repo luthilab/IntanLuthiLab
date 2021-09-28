c7fbcb341bf105cdd118615dd1e014edb7ece9a1
Romain Cardis <58259490+Romain2-5@users.noreply.github.com>
2021-09-28 12:56:24 +0200

    Update findBouts.m

--- PSA_analysis-example/analysis_functions/findBouts.m
+++ PSA_analysis-example/analysis_functions/findBouts.m
@@ -2,8 +2,11 @@ function [Bout] = findBouts(b, type, minSize)
 % give back the begining and the end of bout of 'type' of mininum the size 
 % of 'size' found in b.
 % x is a string or a 1xn matrix. 
-% type is a 1x1 string or one number
-% size is a number
+% type is one element of b that compose the bout of interest.
+% minSize is a number representing the miminal amount of type to be considered a bout.
+% 
+% Romain Cardis 2021
+
 
 ex = sprintf('[^%s]%s{%i,}[^%s]', type, type, minSize, type);
 st = regexp(b, ex, 'start')+1;
